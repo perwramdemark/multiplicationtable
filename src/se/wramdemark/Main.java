6131d6feb91650e96fe34c3b35b27de6e15a6907
Per Wramdemark <per@wramdemark.se>
2019-03-09 10:36:50 +0100

    Add multipliationtable

---
+++ src/se/wramdemark/Main.java
@@ -0,0 +1,22 @@
+package se.wramdemark;
+
+import java.util.stream.IntStream;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int range = 10;
+        System.out.print("  x|");
+        IntStream.range(1, range + 1).forEach(z -> {
+            System.out.print(String.format("%4d|", z));
+        });
+        System.out.println("\n------------------------------------------------------");
+        IntStream.range(1, range).forEach(y -> {
+            System.out.print(String.format("%3d|", y));
+            IntStream.rangeClosed(1, range).forEach(x -> System.out.print(String.format("%4d|", y * x)));
+            System.out.println("");
+        }
+        );
+
+    }
+}
