1a4ac7ddea67a86878366e71622a343167d24fe9
Per Wramdemark <per@wramdemark.se>
2019-03-09 10:54:28 +0100

    Fix line

--- src/se/wramdemark/Main.java
+++ src/se/wramdemark/Main.java
@@ -10,8 +10,10 @@ public class Main {
         IntStream.range(1, range + 1).forEach(z -> {
             System.out.print(String.format("%4d|", z));
         });
-        System.out.println("\n------------------------------------------------------");
-        IntStream.range(1, range).forEach(y -> {
+        System.out.println("");
+        IntStream.range(1, range + 2).forEach(b -> System.out.print("-----"));
+        System.out.println("");
+        IntStream.range(1, range + 1).forEach(y -> {
             System.out.print(String.format("%3d|", y));
             IntStream.rangeClosed(1, range).forEach(x -> System.out.print(String.format("%4d|", y * x)));
             System.out.println("");
